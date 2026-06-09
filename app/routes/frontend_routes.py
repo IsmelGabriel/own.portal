9f4feec828a6734438b4b6d6319d8d97bd09f9da
Ismel Salazar <ssismel28@gmail.com>
2026-06-08 19:58:29 -0500

    Add user creation view and form; update dashboard link to new user creation

--- app/routes/frontend_routes.py
+++ app/routes/frontend_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, make_response
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt, unset_jwt_cookies
 from app.models.user import User
+from app.models.role import Role
+from app.utils.decorators import require_role
 
 frontend_bp = Blueprint('frontend', __name__)
 
@@ -25,3 +27,10 @@ def logout():
     response = make_response(redirect(url_for('frontend.login')))
     unset_jwt_cookies(response)
     return response
+
+@frontend_bp.route('/users/new')
+@jwt_required()
+@require_role('admin')
+def create_user_view():
+    roles = Role.query.all()
+    return render_template('create_user.html', roles=roles)
